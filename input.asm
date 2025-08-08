1d9e779c7ad3aad87b26f94e98709c761b3e1937
MuhIkhsanH <153172456+MuhIkhsanH@users.noreply.github.com>
2025-08-09 02:53:55 +0700

    Create input.asm

---
+++ input.asm
@@ -0,0 +1,52 @@
+.model small
+.stack 100h
+
+.data
+prompt db 'Masukkan Nama: $'
+newline db 13,10,'$'
+buffer db 20
+db 0
+db 20 dup(0)
+
+
+output_msg db 'Hello, $'
+
+
+.code
+main:
+mov ax, @data
+mov ds, ax
+
+mov ah, 09h
+lea dx, prompt
+int 21h
+
+mov ah, 0Ah
+lea dx, buffer
+int 21h
+
+mov ah, 09h
+lea dx, newline
+int 21h
+
+mov ah, 09h
+lea dx, output_msg
+int 21h
+
+mov si, offset buffer
+mov cl, [si+1]
+xor ch, ch
+mov bx, si
+add bx, 2
+add bx, cx
+mov byte ptr [bx], '$'
+
+lea dx, buffer+2
+mov ah, 09h
+int 21h
+
+mov ah, 4Ch
+int 21h
+
+
+end main
